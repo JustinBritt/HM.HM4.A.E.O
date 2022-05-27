740051424c1d966685ef37e28ae45dfda89daf70
Justin Britt <britt@uwindsor.ca>
2022-05-27 10:30:08 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/ResultElements/Sets/S2ResultElement.cs
@@ -0,0 +1,30 @@
+﻿namespace HM.HM4.A.E.O.Classes.ResultElements.Sets
+{
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+
+    internal sealed class S2ResultElement : IS2ResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S2ResultElement(
+            IrIndexElement rIndexElement,
+            IdIndexElement d1IndexElement,
+            IdIndexElement d2IndexElement)
+        {
+            this.rIndexElement = rIndexElement;
+
+            this.d1IndexElement = d1IndexElement;
+
+            this.d2IndexElement = d2IndexElement;
+        }
+
+        public IrIndexElement rIndexElement { get; }
+
+        public IdIndexElement d1IndexElement { get; }
+
+        public IdIndexElement d2IndexElement { get; }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/ResultElements/Sets/IS2ResultElement.cs
@@ -0,0 +1,13 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.ResultElements.Sets
+{
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    public interface IS2ResultElement
+    {
+        IrIndexElement rIndexElement { get; }
+
+        IdIndexElement d1IndexElement { get; }
+
+        IdIndexElement d2IndexElement { get; }
+    }
+}
