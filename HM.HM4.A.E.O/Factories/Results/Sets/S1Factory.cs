f1b37b394183a2568e2edb3828235f363d675e3f
Justin Britt <britt@uwindsor.ca>
2022-05-27 13:57:03 -0400

    Added factory

---
+++ HM.HM4.A.E.O/Factories/Results/Sets/S1Factory.cs
@@ -0,0 +1,41 @@
+﻿namespace HM.HM4.A.E.O.Factories.Results.Sets
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Classes.Results.Sets;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+    using HM.HM4.A.E.O.InterfacesFactories.Results.Sets;
+
+    internal sealed class S1Factory : IS1Factory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S1Factory()
+        {
+        }
+
+        public IS1 Create(
+            ImmutableList<IS1ResultElement> value)
+        {
+            IS1 result = null;
+
+            try
+            {
+                result = new S1(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HM.HM4.A.E.O/InterfacesFactories/Results/Sets/IS1Factory.cs
@@ -0,0 +1,13 @@
+﻿namespace HM.HM4.A.E.O.InterfacesFactories.Results.Sets
+{
+    using System.Collections.Immutable;
+
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+
+    public interface IS1Factory
+    {
+        IS1 Create(
+            ImmutableList<IS1ResultElement> value);
+    }
+}
