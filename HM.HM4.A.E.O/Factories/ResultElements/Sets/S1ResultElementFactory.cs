38b71cc3d373778485e2c239e6ded472c9a22465
Justin Britt <britt@uwindsor.ca>
2022-05-27 09:55:54 -0400

    Added factory

---
+++ HM.HM4.A.E.O/Factories/ResultElements/Sets/S1ResultElementFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace HM.HM4.A.E.O.Factories.ResultElements.Sets
+{
+    using System;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Classes.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.InterfacesFactories.ResultElements.Sets;
+
+    internal sealed class S1ResultElementFactory : IS1ResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S1ResultElementFactory()
+        {
+        }
+
+        public IS1ResultElement Create(
+            IrIndexElement rIndexElement)
+        {
+            IS1ResultElement resultElement = null;
+
+            try
+            {
+                resultElement = new S1ResultElement(
+                    rIndexElement);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return resultElement;
+        }
+    }
+}
---
+++ HM.HM4.A.E.O/InterfacesFactories/ResultElements/Sets/IS1ResultElementFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace HM.HM4.A.E.O.InterfacesFactories.ResultElements.Sets
+{
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+
+    public interface IS1ResultElementFactory
+    {
+        IS1ResultElement Create(
+            IrIndexElement rIndexElement);
+    }
+}
