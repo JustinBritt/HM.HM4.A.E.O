80ea80fc97fd70058373df73a48c0ee17f8ed95e
Justin Britt <britt@uwindsor.ca>
2022-05-27 08:28:59 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/ResultElements/Sets/S1ResultElement.cs
@@ -0,0 +1,20 @@
+﻿namespace HM.HM4.A.E.O.Classes.ResultElements.Sets
+{
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+
+    internal sealed class S1ResultElement : IS1ResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S1ResultElement(
+            IrIndexElement rIndexElement)
+        {
+            this.rIndexElement = rIndexElement;
+        }
+
+        public IrIndexElement rIndexElement { get; }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/ResultElements/Sets/IS1ResultElement.cs
@@ -0,0 +1,9 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.ResultElements.Sets
+{
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    public interface IS1ResultElement
+    {
+        IrIndexElement rIndexElement { get; }
+    }
+}
