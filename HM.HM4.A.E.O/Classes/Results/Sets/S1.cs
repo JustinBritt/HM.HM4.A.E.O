b2442ce84cba79968dd7ac1dbefb84fb8e2b910c
Justin Britt <britt@uwindsor.ca>
2022-05-27 13:26:51 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/Results/Sets/S1.cs
@@ -0,0 +1,42 @@
+﻿namespace HM.HM4.A.E.O.Classes.Results.Sets
+{
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+
+    internal sealed class S1 : IS1
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S1(
+            ImmutableList<IS1ResultElement> value)
+        {
+            this.Value = value;
+        }
+
+        public ImmutableList<IS1ResultElement> Value { get; }
+
+        public bool IsThereElementAt(
+            IrIndexElement rIndexElement)
+        {
+            int count = this.Value
+                .Where(x => x.rIndexElement == rIndexElement)
+                .Distinct()
+                .Count();
+
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/Results/Sets/IS1.cs
@@ -0,0 +1,15 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.Results.Sets
+{
+    using System.Collections.Immutable;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    
+    public interface IS1
+    {
+        ImmutableList<IS1ResultElement> Value { get; }
+
+        bool IsThereElementAt(
+            IrIndexElement rIndexElement);
+    }
+}
