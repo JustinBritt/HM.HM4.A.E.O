814b646debcd861c7395f7655a76dba71c8d4778
Justin Britt <britt@uwindsor.ca>
2022-05-28 02:21:37 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/CrossJoinElements/rddCrossJoinElement.cs
@@ -0,0 +1,30 @@
+﻿namespace HM.HM4.A.E.O.Classes.CrossJoinElements
+{
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.CrossJoinElements;
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    internal sealed class rddCrossJoinElement : IrddCrossJoinElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public rddCrossJoinElement(
+            IrIndexElement rIndexElement,
+            IdIndexElement d1IndexElement,
+            IdIndexElement d2IndexElement)
+        {
+            this.rIndexElement = rIndexElement;
+
+            this.d1IndexElement = d1IndexElement;
+
+            this.d2IndexElement = d2IndexElement;
+        }
+
+        public IrIndexElement rIndexElement { get; }
+
+        public IdIndexElement d1IndexElement { get; }
+
+        public IdIndexElement d2IndexElement { get; }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/CrossJoinElements/IrddCrossJoinElement.cs
@@ -0,0 +1,13 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.CrossJoinElements
+{
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    public interface IrddCrossJoinElement
+    {
+        IrIndexElement rIndexElement { get; }
+
+        IdIndexElement d1IndexElement { get; }
+
+        IdIndexElement d2IndexElement { get; }
+    }
+}
