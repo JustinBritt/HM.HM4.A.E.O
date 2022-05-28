9c8e54ffe50e4b9d1a54e46a6967a4ab55370de9
Justin Britt <britt@uwindsor.ca>
2022-05-28 01:53:55 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/Calculations/Sets/S1Calculation.cs
@@ -0,0 +1,48 @@
+﻿namespace HM.HM4.A.E.O.Classes.Calculations.Sets
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.Calculations.Sets;
+    using HM.HM4.A.E.O.Interfaces.Indices;
+    using HM.HM4.A.E.O.Interfaces.Parameters.SurgicalSpecialties;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.SurgeonOperatingRoomDayAssignments;
+    using HM.HM4.A.E.O.InterfacesFactories.ResultElements.Sets;
+    using HM.HM4.A.E.O.InterfacesFactories.Results.Sets;
+
+    internal sealed class S1Calculation : IS1Calculation
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S1Calculation()
+        {
+        }
+
+        public IS1 Calculate(
+            IS1ResultElementFactory S1ResultElementFactory,
+            IS1Factory S1Factory,
+            Ir r,
+            Ix x,
+            IΔ Δ)
+        {
+            return S1Factory.Create(
+                r.Value
+                .Select(
+                    i => Tuple.Create(
+                        i,
+                        x.Value
+                        .Where(j => j.rIndexElement == i && j.Value)
+                        .Select(j => j.sIndexElement)
+                        .Select(j => Δ.GetSurgicalSpecialtyOfSurgeon(j).Value)
+                        .Distinct()
+                        .Count()))
+                .Where(i => i.Item2 > 1)
+                .Select(i => S1ResultElementFactory.Create(i.Item1))
+                .ToImmutableList());
+        }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/Calculations/Sets/IS1Calculation.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.Calculations.Sets
+{
+    using HM.HM4.A.E.O.Interfaces.Indices;
+    using HM.HM4.A.E.O.Interfaces.Parameters.SurgicalSpecialties;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.SurgeonOperatingRoomDayAssignments;
+    using HM.HM4.A.E.O.InterfacesFactories.ResultElements.Sets;
+    using HM.HM4.A.E.O.InterfacesFactories.Results.Sets;
+
+    public interface IS1Calculation
+    {
+        IS1 Calculate(
+            IS1ResultElementFactory S1ResultElementFactory,
+            IS1Factory S1Factory,
+            Ir r,
+            Ix x,
+            IΔ Δ);
+    }
+}
