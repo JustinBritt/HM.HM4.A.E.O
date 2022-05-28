fa54794b9422a9e82d20a821a89255afb800346c
Justin Britt <britt@uwindsor.ca>
2022-05-28 01:26:24 -0400

    Added factory

---
+++ HM.HM4.A.E.O/Factories/Results/Sets/S2Factory.cs
@@ -0,0 +1,41 @@
+﻿namespace HM.HM4.A.E.O.Factories.Results.Sets
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Classes.Results.Sets;
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+    using HM.HM4.A.E.O.InterfacesFactories.Results.Sets;
+
+    internal sealed class S2Factory : IS2Factory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public S2Factory()
+        {
+        }
+
+        public IS2 Create(
+            ImmutableList<IS2ResultElement> value)
+        {
+            IS2 result = null;
+
+            try
+            {
+                result = new S2(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HM.HM4.A.E.O/InterfacesFactories/Results/Sets/IS2Factory.cs
@@ -0,0 +1,13 @@
+﻿namespace HM.HM4.A.E.O.InterfacesFactories.Results.Sets
+{
+    using System.Collections.Immutable;
+
+    using HM.HM4.A.E.O.Interfaces.ResultElements.Sets;
+    using HM.HM4.A.E.O.Interfaces.Results.Sets;
+   
+    public interface IS2Factory
+    {
+        IS2 Create(
+            ImmutableList<IS2ResultElement> value);
+    }
+}
