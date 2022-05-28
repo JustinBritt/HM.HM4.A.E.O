642ae5bed77fbf96bac43a2b201da3ce68a49927
Justin Britt <britt@uwindsor.ca>
2022-05-28 02:29:43 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/CrossJoins/rdd.cs
@@ -0,0 +1,22 @@
+﻿namespace HM.HM4.A.E.O.Classes.CrossJoins
+{
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.CrossJoinElements;
+    using HM.HM4.A.E.O.Interfaces.CrossJoins;
+
+    internal sealed class rdd : Irdd
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public rdd(
+            ImmutableList<IrddCrossJoinElement> value)
+        {
+            this.Value = value;
+        }
+
+        public ImmutableList<IrddCrossJoinElement> Value { get; }
+    }
+}
