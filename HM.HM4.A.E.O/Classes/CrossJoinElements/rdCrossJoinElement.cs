57526e27b37c0b20f1910601215b823e50de0c9f
Justin Britt <britt@uwindsor.ca>
2022-05-28 02:07:11 -0400

    Added class

---
+++ HM.HM4.A.E.O/Classes/CrossJoinElements/rdCrossJoinElement.cs
@@ -0,0 +1,25 @@
+﻿namespace HM.HM4.A.E.O.Classes.CrossJoinElements
+{
+    using log4net;
+
+    using HM.HM4.A.E.O.Interfaces.CrossJoinElements;
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    internal sealed class rdCrossJoinElement : IrdCrossJoinElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public rdCrossJoinElement(
+            IrIndexElement rIndexElement,
+            IdIndexElement dIndexElement)
+        {
+            this.rIndexElement = rIndexElement;
+
+            this.dIndexElement = dIndexElement;
+        }
+
+        public IrIndexElement rIndexElement { get; }
+
+        public IdIndexElement dIndexElement { get; }
+    }
+}
---
+++ HM.HM4.A.E.O/Interfaces/CrossJoinElements/IrdCrossJoinElement.cs
@@ -0,0 +1,11 @@
+﻿namespace HM.HM4.A.E.O.Interfaces.CrossJoinElements
+{
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+
+    public interface IrdCrossJoinElement
+    {
+        IrIndexElement rIndexElement { get; }
+
+        IdIndexElement dIndexElement { get; }
+    }
+}
