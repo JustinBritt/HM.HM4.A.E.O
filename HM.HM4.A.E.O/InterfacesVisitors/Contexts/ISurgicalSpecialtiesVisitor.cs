e542227b0c20866389cc0341be242880511ae2b4
Justin Britt <britt@uwindsor.ca>
2023-06-15 07:11:26 -0400

    Added visitor

---
+++ HM.HM4.A.E.O/InterfacesVisitors/Contexts/ISurgicalSpecialtiesVisitor.cs
@@ -0,0 +1,22 @@
+﻿namespace HM.HM4.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ParameterElements.SurgicalSpecialties;
+
+    public interface ISurgicalSpecialtiesVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : IImmutableSet<Organization>
+    {
+        RedBlackTree<IjIndexElement, ImmutableList<IsIndexElement>> RedBlackTree { get; }
+
+        List<IΔParameterElement> Value { get; }
+    }
+}
