63e2eb1a5e51838ae6550addeede97880b87e6d2
Justin Britt <britt@uwindsor.ca>
2023-06-15 09:00:58 -0400

    Added factory

---
+++ HM.HM4.A.E.O/InterfacesFactories/Comparers/IFhirDateTimeComparerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace HM.HM4.A.E.O.InterfacesFactories.Comparers
+{
+    using HM.HM4.A.E.O.Interfaces.Comparers;
+
+    public interface IFhirDateTimeComparerFactory
+    {
+        IFhirDateTimeComparer Create();
+    }
+}
