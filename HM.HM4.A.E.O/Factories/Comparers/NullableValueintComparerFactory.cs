325b91d5c1fea4382baa433de545ee9d61867305
Justin Britt <britt@uwindsor.ca>
2023-06-15 06:44:35 -0400

    Added factory

---
+++ HM.HM4.A.E.O/Factories/Comparers/NullableValueintComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM4.A.E.O.Factories.Comparers
+{
+    using HM.HM4.A.E.O.Classes.Comparers;
+    using HM.HM4.A.E.O.Interfaces.Comparers;
+    using HM.HM4.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class NullableValueintComparerFactory : INullableValueintComparerFactory
+    {
+        public NullableValueintComparerFactory()
+        {
+        }
+
+        public INullableValueintComparer Create()
+        {
+            INullableValueintComparer instance = null;
+
+            try
+            {
+                instance = new NullableValueintComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
