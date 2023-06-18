816bbd334bf7cabeb6b719db0c6e925e1782ff38
Justin Britt <britt@uwindsor.ca>
2023-06-18 10:25:57 -0400

    Added visitor

---
+++ HM.HM4.A.E.O/InterfacesVisitors/Contexts/IScenarioProbabilitiesVisitor.cs
@@ -0,0 +1,15 @@
+﻿namespace HM.HM4.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ParameterElements.ScenarioProbabilities;
+
+    public interface IScenarioProbabilitiesVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+    {
+        RedBlackTree<IΛIndexElement, IΡParameterElement> RedBlackTree { get; }
+    }
+}
