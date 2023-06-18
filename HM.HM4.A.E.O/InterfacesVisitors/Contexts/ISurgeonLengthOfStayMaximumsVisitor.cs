0057b3234caecaab56ec86f209ef45b71b9d241a
Justin Britt <britt@uwindsor.ca>
2023-06-18 17:03:22 -0400

    Added visitor

---
+++ HM.HM4.A.E.O/InterfacesVisitors/Contexts/ISurgeonLengthOfStayMaximumsVisitor.cs
@@ -0,0 +1,15 @@
+﻿namespace HM.HM4.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ParameterElements.SurgeonLengthOfStayMaximums;
+
+    public interface ISurgeonLengthOfStayMaximumsVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+    {
+        RedBlackTree<IsIndexElement, ILParameterElement> RedBlackTree { get; }
+    }
+}
