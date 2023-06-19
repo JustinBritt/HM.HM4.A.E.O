85a5e8453fbd18b57b50137c68cea9f084863e7f
Justin Britt <britt@uwindsor.ca>
2023-06-19 11:14:25 -0400

    Added visitor

---
+++ HM.HM4.A.E.O/InterfacesVisitors/Contexts/ISurgeonScenarioMaximumNumberPatientMeansOuterVisitor.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM4.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM4.A.E.O.Interfaces.IndexElements;
+    using HM.HM4.A.E.O.Interfaces.ParameterElements.SurgeonScenarioMaximumNumberPatients;
+
+    public interface ISurgeonScenarioMaximumNumberPatientMeansOuterVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+    {
+        RedBlackTree<IsIndexElement, RedBlackTree<IΛIndexElement, IμParameterElement>> RedBlackTree { get; }
+    }
+}
