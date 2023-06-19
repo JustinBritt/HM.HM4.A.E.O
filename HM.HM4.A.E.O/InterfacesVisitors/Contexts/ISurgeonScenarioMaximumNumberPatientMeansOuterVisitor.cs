4ee795eb0653cf80c289f2fb2b1b99f1817b3404
Justin Britt <britt@uwindsor.ca>
2023-06-19 11:18:39 -0400

    Added constraints

--- HM.HM4.A.E.O/InterfacesVisitors/Contexts/ISurgeonScenarioMaximumNumberPatientMeansOuterVisitor.cs
+++ HM.HM4.A.E.O/InterfacesVisitors/Contexts/ISurgeonScenarioMaximumNumberPatientMeansOuterVisitor.cs
@@ -11,6 +11,8 @@
     using HM.HM4.A.E.O.Interfaces.ParameterElements.SurgeonScenarioMaximumNumberPatients;
 
     public interface ISurgeonScenarioMaximumNumberPatientMeansOuterVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : RedBlackTree<INullableValue<int>, INullableValue<decimal>>
     {
         RedBlackTree<IsIndexElement, RedBlackTree<IΛIndexElement, IμParameterElement>> RedBlackTree { get; }
     }
