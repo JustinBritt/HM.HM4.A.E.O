4ab88b65ec42871c141d31ebaacb7b1f3bb0d320
Justin Britt <britt@uwindsor.ca>
2023-06-20 10:19:33 -0400

    Changes to usings

--- HM.HM4.A.E.O/Visitors/Contexts/SurgeonDayScenarioCumulativeNumberPatientsSecondInnerVisitor.cs
+++ HM.HM4.A.E.O/Visitors/Contexts/SurgeonDayScenarioCumulativeNumberPatientsSecondInnerVisitor.cs
@@ -7,7 +7,6 @@
     using Hl7.Fhir.Model;
 
     using NGenerics.DataStructures.Trees;
-    using NGenerics.Patterns.Visitor;
 
     using HM.HM4.A.E.O.Interfaces.IndexElements;
     using HM.HM4.A.E.O.Interfaces.Indices;
