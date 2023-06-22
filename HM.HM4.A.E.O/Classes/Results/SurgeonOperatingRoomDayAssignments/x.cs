c9f28059acaf12ec55132304666fb25195e415f6
Justin Britt <britt@uwindsor.ca>
2023-06-22 13:22:54 -0400

    Changes to usings

--- HM.HM4.A.E.O/Classes/Results/SurgeonOperatingRoomDayAssignments/x.cs
+++ HM.HM4.A.E.O/Classes/Results/SurgeonOperatingRoomDayAssignments/x.cs
@@ -8,6 +8,8 @@
 
     using Hl7.Fhir.Model;
 
+    using NGenerics.DataStructures.Trees;
+
     using HM.HM4.A.E.O.Interfaces.IndexElements;
     using HM.HM4.A.E.O.Interfaces.ResultElements.SurgeonOperatingRoomDayAssignments;
     using HM.HM4.A.E.O.Interfaces.Results.SurgeonOperatingRoomDayAssignments;
